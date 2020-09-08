6752b65f0a2544365472645c93fc016c2fec707c
Joaquin Mellado Quevedo <mellado.joaquin@gmail.com>
2020-09-08 13:27:57 -0300

    rm methods starting again testing data api

--- components.rb
+++ components.rb
@@ -1,78 +1,17 @@
-
-def head
-    "<!DOCTYPE html>
-    <html lang='en'>
-        <head>
-            <meta charset='UTF-8'>
-            <meta name='viewport' content='width=device-width, initial-scale=1.0'>
-            <title>Document</title>
-            <!--CSS de Bootstrap 4-->
-            <link rel='stylesheet' href='https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css' integrity='sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk' crossorigin='anonymous'>
-            <!--Font Awesome-->
-            <link rel='stylesheet' href='https://use.fontawesome.com/releases/v5.13.1/css/all.css' integrity='sha384-xxzQGERXS00kBmZW/6qxqJPyxW3UR0BPsL4c8ILaIWXva5kFi7TxkIIaMiKtqV1Q' crossorigin='anonymous'>
-            <!--Mi Estilo-->
-            <link rel='stylesheet' href='./assets/css/style.css'>
-        </head>
-        <body>"
-end
-
-
-def request( url, api_key )
-
-
-    require "uri"
-    require "net/http"
-
-
-    https = Net::HTTP.new(url.host, url.port);
-    https.use_ssl = true
-
-    request = Net::HTTP::Get.new(url)
-
-    response = https.request(request)
-    data = response.read_body
-    data = JSON.parse(data)
-
-end
-
-
-def build_web_page( data )
-    
-    i = 1
-    html = ""
-    ul = ""
-
-    while (i < data)
-        i += 1
-        ul = "<ul>#{html}</ul>"
-        html += "\n\t<li> #{i - 1} </li>\n"
-
-    end
-
-    puts "#{ul}"
-end
-
-# camara_amount = {}
-
-# def photos_count( params )
-    
-# end
-
-def foot
-    "<!--JQuery-->
-    <script src='https://code.jquery.com/jquery-3.5.1.min.js' integrity='sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=' crossorigin='anonymous'></script>
-    <!--PopperJS-->
-    <script src='https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js' integrity='sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo' crossorigin='anonymous'></script>
-    <!--BootstrapJS-->
-    <script src='https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js' integrity='sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI' crossorigin='anonymous'></script>
-</body>
-</html>"
-end
+require "uri"
+require "net/http"
+require "json"
 
 url = URI("https://api.nasa.gov/planetary/apod?api_key=BRCj48Dlp7DbgVIWzYkRLp1zu60qKE92kfxDH4ec")
 api_key = 'BRCj48Dlp7DbgVIWzYkRLp1zu60qKE92kfxDH4ec'
 
+https = Net::HTTP.new(url.host, url.port);
+https.use_ssl = true
 
+request = Net::HTTP::Get.new(url)
 
-index = head() + build_web_page() + foot()
-File.write('./index.html', index)
+response = https.request(request)
+data = response.read_body
+data_api = JSON.parse(data)
+
+puts data_api.class
