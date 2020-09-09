6bd4d64ce59046f5e5c12f0a2552d4b1832686b8
Joaquin Mellado Quevedo <mellado.joaquin@gmail.com>
2020-09-09 17:57:28 -0300

    changes in order to understand more every element

--- components.rb
+++ components.rb
@@ -7,7 +7,7 @@ require "json"
 
 def request( url, api_key )
 
-    url = URI("#{url}&api_key=#{api_key}")
+    url = URI( "#{ url }&api_key=#{ api_key }" )
 
     https = Net::HTTP.new(url.host, url.port);
     https.use_ssl = true
@@ -18,13 +18,13 @@ def request( url, api_key )
     data = response.read_body
     data_api = JSON.parse(data)
 
-    data_photos = data_api["photos"]
+    data_photos = data_api[ "photos" ]
 
     data_imgs = []
 
-    data_photos.each do | e |
-        e.each do | key, value |
-            data_imgs.push( value ) if key == "img_src"
+    data_photos.each do | iter |
+        iter.each do | key, value |
+            data_imgs.push( value ) if ( key == "img_src" )
         end
     end
     return data_imgs
@@ -32,6 +32,7 @@ end
 
 
 def build_web_page( data_imgs )
+
     first_part = "<html>\n  <head>\n  </head>\n <body>\n"
     middle = "    <ul>"
     last_part = "    </ul>\n</body>\n</html>"
@@ -39,17 +40,15 @@ def build_web_page( data_imgs )
     data_imgs.each do | value |
         middle += "\n       <li><img src=\"#{ value }\"></img></li>\n"
     end
-    return first_part + middle + last_part
+    return ( first_part + middle + last_part )
 end
 
-#conecting everything adding file
 
-params_request = request("https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000", "BRCj48Dlp7DbgVIWzYkRLp1zu60qKE92kfxDH4ec")
-create_web = build_web_page( params_request )
+params_request = request( "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000", "BRCj48Dlp7DbgVIWzYkRLp1zu60qKE92kfxDH4ec" )
+index = build_web_page( params_request )
 
-index = create_web 
 
-File.write('./index.html', index)
+File.write( './index.html', index )
 
 
 
