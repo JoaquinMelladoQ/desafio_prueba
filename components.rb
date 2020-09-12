872995608aa4f030afd9305d4ef53da8b02eccd8
Joaquin Mellado Quevedo <mellado.joaquin@gmail.com>
2020-09-11 21:39:18 -0300

    still working but added few items to reach data

--- components.rb
+++ components.rb
@@ -16,16 +16,28 @@ def request( url, api_key )
 
     data_photos = data_api[ "photos" ]
     data_imgs = []
+    # data_name = []
     data_photos.each do | iter |
         iter.each do | key, value |
             data_imgs.push( value ) if ( key == "img_src" )
+            # data_name.push ( value["name"] ) if key == "camera"
         end
     end
-    return data_imgs    
+    # return data_name    
+    return data_imgs
     
 end
 
 params_request = request( "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000", "BRCj48Dlp7DbgVIWzYkRLp1zu60qKE92kfxDH4ec" )
+
+# data_name = []
+# params_request["photos"].each do | iter |
+#     iter.each do | key, value |
+#         data_name.push ( value["name"] ) if key == "camera"
+#     end
+# end
+
+# puts data_name
 
 
 def build_web_page( data_imgs )
